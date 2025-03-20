2b1ed3010bd82672217f19e31a1eb67f653fdd0a
Zan-Kir <murilo4santos@hotmail.com>
2025-03-19 22:11:37 -0300

    feat: biscoito da sorte mobile

--- ProgDispMovel/introducao/projeto_mobile/App.js
+++ ProgDispMovel/introducao/projeto_mobile/App.js
@@ -1,50 +1,39 @@
 import React, { Component } from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image, TextInput } from "react-native";
-import imagem from "./assets/steve.png";
-import joia from "./assets/joia.jpg";
+import { StyleSheet, Text, View, Image, TextInput, Button } from "react-native";
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      nome: 'Murilo'
+      nome: '',
+      input: ''
     };
+
+    this.entrar = this.entrar.bind(this);
+  }
+
+  entrar() {
+    if (this.state.input === '') {
+        alert('Digite seu nome!');
+        return;
+    } else {
+        this.setState({ nome: 'Bem vindo: ' + this.state.input });
+    }
   }
 
   render() {
-    return(
+    return (
       <View style={styles.container}>
-        <TextInput style={styles.input}/>
-        <Text style={styles.texto}>Bem Vindo</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Digite seu nome?"
+          underlineColorAndroid="transparent"
+          onChangeText={(texto) => this.setState({input: texto})}
+        />
+        <Button title="Entrar" onPress={this.entrar} />
+        <Text style={styles.texto}>{this.state.nome}</Text>
       </View>
     );
-
-    // return(
-    //   <View style={{flex:1, flexDirection: 'column', alignItems: 'stretch', justifyContent:"space-around"}}>
-
-    //   <View style={{width: 100, height: 50, backgroundColor: 'red'}}></View>
-    //   <View style={{height: 100, backgroundColor: 'green'}}></View>
-    //   <View style={{height: 100,  backgroundColor: 'yellow'}}></View>
-    //   <View style={{height: 100,  backgroundColor: 'blue'}}></View>
-
-    //   </View>
-    // )
-
-    // return (
-    //   <View style={{ marginTop: 20 }}>
-    //     <Text style={styles.titulo}> Boa noite </Text>
-    //     <Text style={styles.textoPrincipal}>
-    //       {" "}
-    //       Teste de introdução a React Native{" "}
-    //     </Text>
-    //     <Text style={styles.textoSecundario}> Tuts tuts </Text>
-    //     <Image source={imagem} style={{ width: 200, height: 200 }} />
-    //     <Text style={styles.textoImagem}> {nome} </Text>
-    //     <Image source={joia} style={{ width: 200, height: 200 }} />
-    //   </View>
-    // );
   }
 }
 
@@ -55,30 +44,14 @@ const styles = StyleSheet.create({
   input: {
     height: 46,
     borderWidth: 1,
-    borderColor: '#222',
+    borderColor: "#222",
     margin: 10,
     fontSize: 20,
+    padding: 10,
   },
-  titulo: {
-    fontSize: 35,
-    color: "black"
-  },
-
-  textoPrincipal: {
-    fontSize: 30,
-    color: "green",
-  },
-
-  textoSecundario: {
-    color: "#FF0000",
+  texto: {
+    textAlign: "center",
     fontSize: 25,
-    margin: 15,
-  },
-
-  textoImagem: {
-    marginLeft: 70,
-    fontSize: 20,
-    color: "blue",
   },
 });
 
