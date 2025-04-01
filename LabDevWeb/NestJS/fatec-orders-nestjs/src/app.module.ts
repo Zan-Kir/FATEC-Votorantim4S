9cc2660fdd4afa2d708e5d6b66b138a1dd84b06d
Zan-Kir <murilo4santos@hotmail.com>
2025-03-31 21:41:25 -0300

    feat: sequelize

--- LabDevWeb/NestJS/fatec-orders-nestjs/src/app.module.ts
+++ LabDevWeb/NestJS/fatec-orders-nestjs/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ProductsModule], //importa outros modulos
+  imports: [ProductsModule, DatabaseModule], //importa outros modulos
   controllers: [], // inclui controladores
   providers: [], // inclui serviços e repositórios
   exports: [], // exporta recursos para outros módulos
