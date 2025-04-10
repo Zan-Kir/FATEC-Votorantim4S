9009419ab6483afde92be3dee8e5d52c80eda82f
Zan-Kir <murilo4santos@hotmail.com>
2025-04-09 22:09:50 -0300

    feat: useState react native

--- InteEntCont/DockerVolume/routes.js
+++ InteEntCont/DockerVolume/routes.js
@@ -1,35 +1,35 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 let usuarios = [
-  { id: 1, nome: 'João', email: 'joao@email.com' },
-  { id: 2, nome: 'Maria', email: 'maria@email.com' },
-  { id: 3, nome: 'Pedro', email: 'pedro@email.com' }
+  { id: 1, nome: "João", email: "joao@email.com" },
+  { id: 2, nome: "Maria", email: "maria@email.com" },
+  { id: 3, nome: "Pedro", email: "pedro@email.com" },
 ];
 
-router.get('/usuarios', (req, res) => {
+router.get("/usuarios", (req, res) => {
   res.json(usuarios);
 });
 
-router.post('/usuarios', (req, res) => {
+router.post("/usuarios", (req, res) => {
   const { nome, email } = req.body;
 
   if (!nome || !email) {
-    return res.status(400).json({ message: 'Nome e e-mail são obrigatórios!' });
+    return res.status(400).json({ message: "Nome e e-mail são obrigatórios!" });
   }
 
   const novoUsuario = {
     id: usuarios.length + 1,
     nome,
-    email
+    email,
   };
 
   usuarios.push(novoUsuario);
   res.status(201).json(novoUsuario);
 });
 
-router.get('/ok', (req, res) => {
-  res.status(200).json({ message: 'OK' });
+router.get("/ok", (req, res) => {
+  res.status(200).json({ message: "OK" });
 });
 
 module.exports = router;
